40a7b114d808dbf4ad55395523ef8f7e8eab2563
xi.chen <xi.chen@mxi.ai>
2025-04-29 15:49:54 +0800

    one dark vivid

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- 禁用相对行号，只使用绝对行号
+    vim.opt.relativenumber = false
+    vim.opt.number = true  -- 保留绝对行号
+end
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,5 +3,12 @@ return {
     {
         "olimorris/onedarkpro.nvim",
         priority = 1000, -- Ensure it loads first
-    }
+        opts = {
+            theme = "onedark_vivid", -- 设置为 One Dark Vivid 主题
+        },
+        config = function(_, opts)
+            require("onedarkpro").setup(opts)
+            vim.cmd.colorscheme("onedark_vivid")
+        end,
+    },
 }
