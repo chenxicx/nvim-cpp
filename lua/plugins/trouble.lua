f2067b216a64510ce9df6db4e30f56b6a56a6353
xi.chen <xi.chen@mxi.ai>
2025-04-29 10:55:58 +0800

    trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,48 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- 默认配置，您可以根据需要调整
+    position = "bottom", -- 在底部显示
+    height = 10, -- 高度为10行
+    icons = true,
+    mode = "workspace_diagnostics", -- 默认模式为工作区诊断
+    fold_open = "", -- 图标：打开的文件夹
+    fold_closed = "", -- 图标：关闭的文件夹
+    group = true, -- 按文件分组
+    padding = true, -- 添加额外的填充
+    action_keys = {
+      -- 自定义按键，这些是默认值
+      close = "q", -- 关闭trouble窗口
+      cancel = "<esc>", -- 取消
+      refresh = "r", -- 刷新
+      jump = { "<cr>", "<tab>" }, -- 跳转到问题
+      open_split = { "<c-x>" }, -- 在分割窗口中打开
+      open_vsplit = { "<c-v>" }, -- 在垂直分割窗口中打开
+      open_tab = { "<c-t>" }, -- 在新标签页中打开
+      jump_close = { "o" }, -- 跳转并关闭
+      toggle_mode = "m", -- 切换模式
+      toggle_preview = "P", -- 预览
+      hover = "K", -- 查看诊断信息
+      preview = "p", -- 预览位置
+      close_folds = { "zM", "zm" }, -- 关闭所有折叠
+      open_folds = { "zR", "zr" }, -- 打开所有折叠
+      toggle_fold = { "zA", "za" }, -- 切换折叠
+      previous = "k", -- 上一个条目
+      next = "j", -- 下一个条目
+    },
+    auto_jump = {},
+    use_diagnostic_signs = true,
+  },
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "打开Trouble" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "工作区诊断" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "文档诊断" },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "位置列表" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "快速修复" },
+    { "<leader>xr", "<cmd>TroubleToggle lsp_references<cr>", desc = "LSP引用" },
+    { "gR", "<cmd>TroubleToggle lsp_references<cr>", desc = "显示引用" },
+    { "[q", function() require("trouble").previous({skip_groups = true, jump = true}) end, desc = "上一个问题" },
+    { "]q", function() require("trouble").next({skip_groups = true, jump = true}) end, desc = "下一个问题" },
+  },
+}
