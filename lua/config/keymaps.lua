5be2b2444ff4ebe2181267ef7e7d07fc07279288
chenxi <chenxicx1992@gmail.com>
2025-05-02 17:04:05 +0800

    C+\打开关闭teminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,6 +11,39 @@ vim.keymap.set("n", "<leader>gg", function()
   vim.cmd("term lazygit")
 end, { desc = "打开 lazygit" })
 
+-- 配置Ctrl+`打开终端，再次按下则折叠终端
+vim.api.nvim_set_keymap("n", [[<C-\>]], [[:lua ToggleTerminal()<CR>]], { noremap = true, silent = true, desc = "打开/折叠终端" })
+vim.api.nvim_set_keymap("t", [[<C-\>]], [[<C-\><C-n>:lua ToggleTerminal()<CR>]], { noremap = true, silent = true, desc = "打开/折叠终端" })
+
+-- 定义终端切换函数
+function _G.ToggleTerminal()
+  -- 检查是否有终端缓冲区存在
+  local terminal_bufnr = nil
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buftype == "terminal" then
+      terminal_bufnr = buf
+      break
+    end
+  end
+  
+  -- 检查当前缓冲区是否为终端
+  if vim.bo.buftype == "terminal" then
+    -- 如果在终端中，则隐藏终端
+    vim.cmd("hide")
+  else
+    if terminal_bufnr then
+      -- 如果已有终端，则显示它
+      vim.cmd("botright 15sp")
+      vim.api.nvim_set_current_buf(terminal_bufnr)
+      vim.cmd("startinsert")
+    else
+      -- 如果不存在终端，则创建新终端
+      vim.cmd("botright 15sp | term")
+      vim.cmd("startinsert")
+    end
+  end
+end
+
 -- 添加格式化选中代码的快捷键
 vim.keymap.set("v", "<leader>cf", function()
   local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
