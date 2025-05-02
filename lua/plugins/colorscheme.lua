6dc78d354e6d7ca4d97788e81fe2df720c6e3854
chenxi <chenxicx1992@gmail.com>
2025-05-02 11:58:33 +0800

    one dark pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,5 +2,14 @@ return
 {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
+  config = function()
+    require("onedarkpro").setup({
+      options = {
+        transparency = false, -- 如果您需要透明背景，可以设置为 true
+      }
+    })
+    -- 设置主题为 onedark_vivid
+    vim.cmd("colorscheme onedark_vivid")
+  end,
 }
 
