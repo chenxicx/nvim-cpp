3380841dbc28ad36a9b88de638c74811cb05111c
chenxi <chenxicx1992@gmail.com>
2025-05-02 11:43:13 +0800

    onddark pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,6 @@
--- 安装 onedark 主题并设置为默认
-return {
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("onedark").setup({
-        style = "deep",
-      })
-      -- Enable theme
-      require("onedark").load()
-    end,
-  },
-
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
-
+return
+{
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
 }
+
