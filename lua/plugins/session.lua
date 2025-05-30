72ef52138fe3ec2f1ce094b86ee5b9ca8f800332
xi.chen <xi.chen@mxi.ai>
2025-05-30 18:01:11 +0800

    session

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,4 +1,19 @@
 return
-{
-  {'Shatur/neovim-session-manager'}
+ {
+    'jedrzejboczar/possession.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+
+    config = function()
+      require('possession').setup {
+        commands = {
+          save = 'SSave',
+          load = 'SLoad',
+          delete = 'SDelete',
+          list = 'SList'
+        }
+      }
+    end
 }
+
